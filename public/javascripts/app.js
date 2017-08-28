b84491285baeffbc18b8f195be6d2f9dff1f79d2
edmoconnor <edmoconnor@gmail.com>
2017-08-28 04:35:15 +0100

    add bootstrap css to tables and made app responsive-ish

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -11,42 +11,40 @@ var app = angular.module('myapp', ['ngMap'])
         
         $http.get('/data', config).then(function(res) {
             var data = JSON.parse(res.data);
-
             $scope.data = data;
-
-            $scope.timeFormat = function (secs) {
-                var hours = Math.floor(secs / (60 * 60));
-
-                var divisor_for_minutes = secs % (60 * 60);
-                var minutes = Math.floor(divisor_for_minutes / 60);
-            
-                var divisor_for_seconds = divisor_for_minutes % 60;
-                var seconds = Math.ceil(divisor_for_seconds);
-            
-                var obj = hours + ":" + minutes + ":" + seconds
-                return obj;
-            }
-
-            $scope.polypath = function(polypath){
-                var path = [];
-                for(var i = 1; i < polypath.path.length; i++){
-                    path.push([polypath.path[i][1], polypath.path[i][0]])
-                }
-               
-                var bounds = new google.maps.LatLngBounds();
-                for (var i=0; i<path.length; i++) {
-                    var latlng = new google.maps.LatLng(path[i][0], path[i][1]);
-                    bounds.extend(latlng);
-                }
-
-                NgMap.getMap().then(function(map) {
-                    map.setCenter(bounds.getCenter());
-                    map.fitBounds(bounds);
-                });
-                return path;
-            }
-
         });
+    }
+
+    $scope.timeFormat = function (secs) {
+        var hours = Math.floor(secs / (60 * 60));
+
+        var divisor_for_minutes = secs % (60 * 60);
+        var minutes = Math.floor(divisor_for_minutes / 60);
+    
+        var divisor_for_seconds = divisor_for_minutes % 60;
+        var seconds = Math.ceil(divisor_for_seconds);
+    
+        var obj = hours + ":" + minutes + ":" + seconds
+        return obj;
+    }
+
+    $scope.polypath = function(polypath){
+        var path = [];
+        for(var i = 1; i < polypath.path.length; i++){
+            path.push([polypath.path[i][1], polypath.path[i][0]])
+        }
+        
+        var bounds = new google.maps.LatLngBounds();
+        for (var i=0; i<path.length; i++) {
+            var latlng = new google.maps.LatLng(path[i][0], path[i][1]);
+            bounds.extend(latlng);
+        }
+
+        NgMap.getMap().then(function(map) {
+            map.setCenter(bounds.getCenter());
+            map.fitBounds(bounds);
+        });
+        return path;
     }
 
     $scope.selActivity=function(activity){
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
     if(!req.user){
-      console.log(req)
       res.redirect("/login");
     }else{
       console.log('index', req.user);
